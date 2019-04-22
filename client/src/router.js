18371357186b36ef729df3e96adcdb56ae3e0a9a
yogapermana76 <permana76.yp@gmail.com>
2019-04-22 18:02:19 +0700

    add board

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import GameBoard from './views/Game-board.vue'
 
 Vue.use(Router)
 
@@ -11,7 +12,12 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      components: Home
+      component: Home
+    },
+    {
+      path: '/game-board',
+      name: 'Game-board',
+      component: GameBoard
     }
   ]
 })
