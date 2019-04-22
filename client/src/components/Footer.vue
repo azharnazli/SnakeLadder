af3f75c90ba40866388dc7287382120a4c72e71e
azharnazli <mazhar.nazli@gmail.com>
2019-04-22 16:10:32 +0700

    add footer/navbar

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-footer fixed dark height="auto">
+    <v-card class="flex" flat tile>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2019 — <strong>SnakeBoard</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-toolbar color="#eaeaea">
+    <v-toolbar-title class="black--text">SnakeBoard</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
